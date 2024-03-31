e18a03b24dda3f7790127edabee1a735bae1489f
SangRyeolBaek <138215275+100-Sang-10@users.noreply.github.com>
2024-03-31 22:27:04 +0900

    Change Following between Waypoint&Sliding_window mid point Again

--- src/control/point_control/src/point_control.cpp
+++ src/control/point_control/src/point_control.cpp
@@ -1,69 +1,4 @@
-#include <ros/ros.h>
-#include <tf/tf.h>
-#include <visualization_msgs/Marker.h>
-#include <carla_msgs/CarlaEgoVehicleControl.h>
-#include <carla_msgs/CarlaEgoVehicleStatus.h>
-#include <nav_msgs/Odometry.h>
-#include <std_msgs/Float32.h>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <cmath>
-#include <geometry_msgs/Point.h>
-#include <geometry_msgs/Pose.h>
-#include <geometry_msgs/PoseArray.h>
-#include <std_msgs/Int64.h>
-
-#define SPEED  30
-
-class PointControl {
-  private:
-    ros::NodeHandle nh;
-    ros::Publisher waypoint_pub;
-    std::vector<std::vector<double>> waypoint_vec;
-    ros::Subscriber current_coordinate_sub;
-    ros::Subscriber current_speed_sub;
-    ros::Publisher control_pub;
-    double angle, co_tf_x, co_tf_y, accelerator;
-    double next_point_x = -54.9099540710449;
-    double next_point_y = 77.2210540771484;
-    int waypoint_count = 0;
-
-    ros::Subscriber sub_midpoint;
-    geometry_msgs::PoseArray midpoint;
-    int following_state = 21;
-    ros::Publisher point_pub;
-    visualization_msgs::Marker line_mid_point;
-    ros::Subscriber sub_state;
-    int state = 0;
-    double mid_point_marker_x = 0.0;
-    double mid_point_marker_y = 0.0;
-    double ego_vehicle_x = 0.0;
-    double ego_vehicle_y = 0.0;
-    double ego_yaw = 0.0;
-
-  public:
-    PointControl();
-    void waypoint();  //waypoint 담은 vector 함수
-    std::vector<std::string> divide(std::string xy_str, char divider);  //벡터에 저장된 문자열 x,y 나눠주는 함수
-    double to_num(std::string s);  //좌표 숫자로 바꿔주는 함수
-    void visualize_waypoint();  //waypoint marker 띄우는 함수
-    void odom_Callback(const nav_msgs::Odometry::ConstPtr& odom_msg);  //현재 좌표와 타겟좌표
-    double cal_yaw(const nav_msgs::Odometry::ConstPtr& odom_msg);  //yaw값 구하는 함수
-    void coordinate_tf();  //좌표 변환 함수
-    void next_point();  //다음 waypoint 
-    void pure_pursuit();
-    void speed_Callback(const std_msgs::Float32::ConstPtr& speed_msg);  //현재 속도
-    void PID(const std_msgs::Float32::ConstPtr& speed_msg);
-    void control();
-
-    void point_Callback(const geometry_msgs::PoseArray::ConstPtr& line_msg);
-    void calc_midpoint();
-    void mid_point_marker();
-    void state_Callback(const std_msgs::Int64::ConstPtr& state_msg);
-};
+#include "point_control.hpp"
 
 PointControl::PointControl() {
     waypoint_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 100);
@@ -354,8 +289,8 @@ void PointControl::mid_point_marker() {
     mid_point_marker_x = d_x + ego_vehicle_x;
     mid_point_marker_y = d_y + ego_vehicle_y;
 
-    ROS_INFO("mid_point_marker_x = %f", mid_point_marker_x);
-    ROS_INFO("mid_point_marker_y = %f", mid_point_marker_y);
+    // ROS_INFO("mid_point_marker_x = %f", mid_point_marker_x);
+    // ROS_INFO("mid_point_marker_y = %f", mid_point_marker_y);
 
     line_mid_point.header.frame_id = "map";
     line_mid_point.header.stamp = ros::Time::now();
@@ -371,10 +306,12 @@ void PointControl::mid_point_marker() {
     // line_mid_point is red
     line_mid_point.color.r = 1.0;
     line_mid_point.color.a = 1.0;
-
-    // TODO: co_tf_x,y를 odom 정보 써서 world 좌표로 변환
-    line_mid_point.pose.position.x = mid_point_marker_x;
-    line_mid_point.pose.position.y = mid_point_marker_y;
+    
+    line_mid_point.points.clear();
+    geometry_msgs::Point point;
+    point.x = mid_point_marker_x;
+    point.y = mid_point_marker_y;
+    line_mid_point.points.push_back(point);
 
     point_pub.publish(line_mid_point);
 }
